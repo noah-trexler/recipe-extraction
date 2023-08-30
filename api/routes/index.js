3e0c8e58a03cfcc3aff716817cb4624da1abcaf1
Noah Trexler 2380503 <noah.trexler@cvshealth.com>
2023-08-30 15:47:09 -0400

    split ingredients

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,6 +3,10 @@ var puppeteer = require("puppeteer");
 var cheerio = require("cheerio");
 var router = express.Router();
 
+const { ingredientSearch } = require("../utils/ingredientSearch");
+const { stepSearch } = require("../utils/stepSearch");
+const { extractIngredients } = require("../utils/extractIngredients");
+
 router.get("/recipe", function (req, res, next) {
   // reference: https://www.freecodecamp.org/news/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3/
   puppeteer
@@ -16,8 +20,9 @@ router.get("/recipe", function (req, res, next) {
     .then((html) => {
       const $ = cheerio.load(html);
 
-      const ingredients = ingredientSearch($);
+      const ingredient_list = ingredientSearch($);
       const steps = stepSearch($);
+      const ingredients = extractIngredients(ingredient_list);
 
       res.status(200).json({
         message: "Successfully retrieved recipe.",
@@ -38,63 +43,3 @@ router.get("/recipe", function (req, res, next) {
 });
 
 module.exports = router;
-
-/**
- * DOM TRAVERSING ALGORITHM
- *
- * Ingredients Section
- * - Search for header with text content 'ingredients'
- * - Traverse up the DOM until an element containing <ul> is found
- * - Extract the text contents of the <ul>
- *
- * Recipe Section
- * - Search for header with text content 'instructions' 'method' 'recipe' 'steps'
- * - Traverse up the DOM until an element containing <ol> is found
- * - Extract the text contents of the <ol>
- */
-
-function ingredientSearch($) {
-  const ingredients = [];
-  $(":is(h1,h2,h3,h4,h5,h6):contains('Ingredients')")
-    .closest(':contains("ul")')
-    .find("ul")
-    .find("li")
-    .each(function (i, elem) {
-      let str = $(this).prop("innerText");
-      ingredients[i] = str.replaceAll(/(<(.|\n)*>|▢)/gm, ""); // RegEx to replace any lingering html tags
-    });
-  return ingredients;
-}
-
-function stepSearch($) {
-  const steps = [];
-
-  // Construct the search terms for traversing DOM
-  const headerTag = ":is(h1,h2,h3,h4,h5,h6):contains";
-  const recipeTitle = [
-    "Method",
-    "Instructions",
-    "Directions",
-    "Preparation",
-    "Recipe",
-    "Technique",
-    "Procedure",
-    "Process",
-  ];
-
-  for (let title of recipeTitle) {
-    let searchString = headerTag + `('${title}')`;
-    const header = $(searchString);
-    if (header.length > 0) {
-      header
-        .closest(':contains("ol")')
-        .find("ol,ul")
-        .find("li")
-        .each(function (i, elem) {
-          let str = $(this).prop("innerText");
-          steps[i] = str.replaceAll(/<(.|\n)*>/gm, "");
-        });
-      return steps;
-    }
-  }
-}
--- recipe-extraction-app/src/app/models/recipe.model.ts
+++ recipe-extraction-app/src/app/models/recipe.model.ts
@@ -1,10 +1,10 @@
 export interface Recipe {
   url: string;
-  // ingredients: {
-  //   ingredient: string;
-  //   amount: number;
-  //   amount_unit: string;
-  // }[];
-  ingredients: string[];
+  ingredients: {
+    ingredient: string;
+    amount: string;
+    amount_unit: string;
+  }[];
+  // ingredients: string[];
   steps: string[];
 }
