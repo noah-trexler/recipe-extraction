13715bcdb28f91abfa21cd35718e78851533a8c0
Noah Trexler 2380503 <noah.trexler@cvshealth.com>
2023-08-31 14:37:17 -0400

    ingredient parsing functionality

--- recipe-extraction-app/src/app/models/recipe.model.ts
+++ recipe-extraction-app/src/app/models/recipe.model.ts
@@ -1,10 +1,34 @@
 export interface Recipe {
   url: string;
-  ingredients: {
-    ingredient: string;
-    amount: string;
-    amount_unit: string;
-  }[];
-  // ingredients: string[];
+  ingredients: Ingredient[];
   steps: string[];
+}
+
+//https://www.npmjs.com/package/parse-ingredient
+
+interface Ingredient {
+  /**
+   * The primary quantity (the lower quantity in a range, if applicable)
+   */
+  quantity: number | null;
+  /**
+   * The secondary quantity (the upper quantity in a range, or `null` if not applicable)
+   */
+  quantity2: number | null;
+  /**
+   * The unit of measure identifier (see `unitsOfMeasure`)
+   */
+  unitOfMeasureID: string | null;
+  /**
+   * The unit of measure
+   */
+  unitOfMeasure: string | null;
+  /**
+   * The description
+   */
+  description: string;
+  /**
+   * Whether the "ingredient" is actually a group header, e.g. "For icing:"
+   */
+  isGroupHeader: boolean;
 }
