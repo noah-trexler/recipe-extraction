19de73a32f09d12560bab9f69ae45b81006f075c
Noah Trexler 2380503 <noah.trexler@cvshealth.com>
2023-09-01 12:18:15 -0400

    chromium install

--- api/.puppeteerrc.cjs
+++ api/.puppeteerrc.cjs
@@ -1,7 +1,7 @@
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // Changes the cache location for Puppeteer.
-  cacheDirectory: "/opt/render/project/src/api/.cache/puppeteer",
-};
+// /**
+//  * @type {import("puppeteer").Configuration}
+//  */
+// module.exports = {
+//   // Changes the cache location for Puppeteer.
+//   cacheDirectory: "/opt/render/project/src/api/.cache/puppeteer",
+// };
